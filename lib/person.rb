076135a32a8fea8d914c6846f10c19a8c50722de
Jacqueline Lee <anginblu@gmail.com>
2017-10-10 00:56:52 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,9 @@
 class Person
-  def @name=(person_name)
-    name = person_name
+  def name=(person_name)
+    @name = person_name
   end
-  def @name
-    person_name
+  def name
+    @name
   end
   def @job=(person_job)
     job = person_job
